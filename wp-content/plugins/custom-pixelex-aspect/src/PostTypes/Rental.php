ebfe0dd10d5171e33d1d193a520f06720df93792
Gemma Black <gblackuk@googlemail.com>
2021-03-26 12:59:31 +0000

    fix: product type taxonomy

--- wp-content/plugins/custom-pixelex-aspect/src/PostTypes/Rental.php
+++ wp-content/plugins/custom-pixelex-aspect/src/PostTypes/Rental.php
@@ -10,12 +10,12 @@ final class Rental
     public function init() : void
     {
         $rentals = new PostType('rental');
-        $rentals->taxonomy('product-type');
+        $rentals->taxonomy('product_type');
         $rentals->columns()->hide([ 'date', 'author' ]);
         $rentals->icon('dashicons-book-alt');
         $rentals->register();
 
-        $productTypes = new Taxonomy('product-type');
+        $productTypes = new Taxonomy('product_type');
         $productTypes->options([
             'hierarchical' => false,
         ]);
